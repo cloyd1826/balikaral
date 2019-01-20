d43f7a0177e0b57e20dac195f313e01d2dea154b
cloyd1826 <alcantaracloyd26@gmail.com>
2019-01-20 08:34:41 +0800

    update config and package

--- src/_config/index.js
+++ src/_config/index.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV === 'production'){
 	config = 'https://balikaralapi.eastus.cloudapp.azure.com:5000'
 }
 
-export const appId = ''
-export const clientId = ''
+export const appId = '521442691675915'
+export const clientId = '197880274985-qae1s30vumso0tbr25iaf9lrk89e3tp7.apps.googleusercontent.com'
 export default config
 
