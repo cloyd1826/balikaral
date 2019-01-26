f3c046f976165d96a2504f1bd24d2be9f1f3b8d4
haru-lyle <rexor.lyle@gmail.com>
2019-01-26 15:40:16 +0900

    Update

--- src/_config/index.js
+++ src/_config/index.js
@@ -8,9 +8,9 @@ if(process.env.NODE_ENV === 'production'){
 	config = 'https://balikaralapi.eastus.cloudapp.azure.com:5000'
 }
 
-export const appId = '344679316117018'
-export const clientId = '293000110428-lm6klam4patr7ojnk0e9md79gkip32jd.apps.googleusercontent.com'
+export const appId = '521442691675915'
+export const clientId = '197880274985-qae1s30vumso0tbr25iaf9lrk89e3tp7.apps.googleusercontent.com'
 export default config
 
-//521442691675915
-//197880274985-qae1s30vumso0tbr25iaf9lrk89e3tp7.apps.googleusercontent.com
+//
+//
