fc86726fd0e1e8221b74ad6234dcf03135c11807
cloydalcantara <alcantaracloyd26@gmail.com>
2020-05-26 14:08:42 +0800

    update

--- src/_config/index.js
+++ src/_config/index.js
@@ -2,10 +2,10 @@
 let config = ''
 
 if(process.env.NODE_ENV === 'development'){
-	config = 'http://13.90.43.83:5000/api'
+	config = 'http://13.90.43.83:5000'
 }
 if(process.env.NODE_ENV === 'production'){
-	config = 'http://127.0.0.1:5000/api'
+	config = 'http://127.0.0.1:5000'
 }
 
 export const appId = '521442691675915'
